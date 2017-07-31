6607d3b279b1535af9a9c9daa9557431610b2269
Constantine112 <768131566@qq.com>
2017-07-31 17:10:05 +0800

    push first day

--- js/log_in_sign_in.js
+++ js/log_in_sign_in.js
@@ -1,3 +1,4 @@
+
 /**
  * [对登录与注册的div的转换]
  */
@@ -28,6 +29,12 @@ $(function() {
     })
 })
 
+/**
+ * [正则表达式对账号密码验证]
+ * 
+ * 
+ * 
+ */
 $(function() {
     
         var username = document.getElementById('username'),
@@ -38,7 +45,7 @@ $(function() {
                 if (!regUser.test(username.value)) {
                     $('.log-in-error').html('用户名由4到8个数字或大小写字母组成').css("display","block");
                 } else {
-                    $('.log-in-error').css("display","none");
+                    $('.log-in-error').html('').css("display","none");
                 }
             })
             $('#password').bind('keyup', function() {
@@ -52,25 +59,28 @@ $(function() {
             })
 })
 
+/**
+ * [登录，对登录信息的上传]
+ *  
+ * 
+ *
+ */
 $(function () {
-    $('.log-submit').bind('click', function () {
+    $('.log-submit').bind('click', function (e) {
         var username = document.getElementById('username'),
             password = document.getElementById('password'),
-            obj = new object();
+            obj = new Object();
             obj.user = username.value;
             obj.password = password.value;
-
+            e.preventDefault(),
             $.ajax({
                 type: "post",
-                url: ,
+                url: 'http://192.168.1.110:10086/hasUser',
                 data: JSON.stringify(CreateJson(obj)),
                 dataType: "json",
                 async: false,
-                // username: 'admin',
-                // password: "admin123456",
                 success: function(data) {
-                    
-
+                    LogInChange(JSON.$.parseJSON(data));
                 },
                 error: function(xhr, status, errorThrowm) {
                     alert("错误" + status + "错误抛出：" + errorThrowm);
@@ -79,10 +89,26 @@ $(function () {
     });
     
 })
+
+/**
+ * [CreateJson 创建JSON数据的对象]
+ * @param {[Object]} obj [存有账号密码等信息的对象]
+ */
 function CreateJson (obj) {
     var account = new Object();
     for (var i in obj) {
         account[i] = obj[i];
     }
     return obj
+}
+
+/**
+ * [LogInChange 登录数据返回对数据的分析]
+ * @param {[type]} data [json解析后的数据]
+ */
+function LogInChange (data) {
+    switch(data.status) {
+        case '1': locaton.href=data.url;break;
+        case '2':  $('.log-in-error').html('用户名不存在或密码错误').css("display","block");
+    }
 }
